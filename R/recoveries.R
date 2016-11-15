acc4eafdd5dba94b90bf0983beb354357f0419ad
bearedo <doug.beare@gmail.com>
2016-11-15 15:15:59 +0100

    metadata for AOTTP recovery table

---
+++ R/recoveries.R
@@ -0,0 +1,25 @@
+#' recoveries
+#' 
+#' Recoveries of tropical tuna in the Atlantic Ocean (example dataset from Atlantic Ocean Tropical Tuna Programme 2015-2020).
+#'
+#' @format A \code{data.frame}
+#' \describe{
+#'   \item{specimenid}{Unique fish ID \code{numeric}}
+#'   \item{rcstagecode}{Stage \code{factor}}
+#'   \item{ctcode1}{ Conventional tag code 1\code{numeric}}
+#'   \item{ctcode2}{ Conventional tag code 2\code{numeric}}
+#'   \item{date}{Recovery date of fish\code{Date}}
+#'   \item{time}{Recovery time of fish \code{factor}}
+#'   \item{latitude}{Latitude of recovery \code{numeric}}
+#'   \item{longitude}{Longitude of recovery \code{numeric}}
+#'   \item{gearcode}{Gearcode used by recovery platform \code{factor}}
+#'   \item{depth}{Depth of release \code{numeric}}
+#'   \item{len}{Length of recovered fish \code{numeric}}
+#' }
+#' 
+#' @references \url{http://http://iccat.int/Documents/CVSP/CV070_2014/n_4/CV070041365.pdf/}
+#' @name recoveries
+#' @docType data
+#' @author Coulo Blanco \email{doug.beare@@gmail.com}
+#' @keywords data
+NULL
