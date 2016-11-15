469ac91552fb747324db1c2ad7ca9efeb9fb50cc
bearedo <doug.beare@gmail.com>
2016-11-15 15:09:49 +0100

    metadata

--- R/releases.R
+++ R/releases.R
@@ -1,20 +1,26 @@
 #' releases
 #' 
-#' An example dataset from Atlantic Ocean Tropical Tuna Programmes. Releases of tropical tuna in the Atlantic Ocean. 
+#' Releases of tropical tuna in the Atlantic Ocean (example dataset from Atlantic Ocean Tropical Tuna Programme 2015-2020.
 #'
 #' @format A \code{data.frame}
 #' \describe{
-#'   \item{sendingid}{Sending ID to identify person who sent the data \code{numeric}}
-#'   \item{specimenid}{Code for individual fish \code{numeric}}
 #'   \item{speciescode}{ICCAT 3 letter species codes \code{character}}
-#'   \item{year}{historical assessment year \code{numeric}}
-#'   \item{stock}{biomaass relative to BMSY \code{numeric}}   
-#'   \item{harvest}{exploitation relative to FMSY \code{numeric}}
+#'   \item{rcstagecode}{Stage \code{numeric}}
+#'   \item{ctcode1}{ Conventional tag code 1\code{numeric}}
+#'   \item{ctcode2}{ Conventional tag code 2\code{numeric}}
+#'   \item{date}{Release date of fish\code{Date}}
+#'   \item{time}{Release time of fish \code{factor}}
+#'   \item{latitude}{Latitude of release \code{numeric}}
+#'   \item{longitude}{Longitude of release \code{numeric}}
+#'   \item{gearcode}{Gearcode used by release platform \code{factor}}
+#'   \item{depth}{Depth of release \code{numeric}}
+#'   \item{len}{Length of fish \code{numeric}}
+#'   \item{specimenid}{Unique fish ID \code{numeric}}
 #' }
 #' 
 #' @references \url{http://http://iccat.int/Documents/CVSP/CV070_2014/n_4/CV070041365.pdf/}
 #' @name releases
 #' @docType data
-#' @author Doug Beare \email{doug.beare@@gmail.com}
+#' @author Coulo Blanco \email{doug.beare@@gmail.com}
 #' @keywords data
 NULL
