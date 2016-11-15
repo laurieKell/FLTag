5db735b155f49fb17ba2e1aa48062e11ac9c12f3
bearedo <doug.beare@gmail.com>
2016-11-15 14:51:52 +0100

    latest

--- R/releases.R
+++ R/releases.R
@@ -6,15 +6,15 @@
 #' \describe{
 #'   \item{sendingid}{Sending ID to identify person who sent the data \code{numeric}}
 #'   \item{specimenid}{Code for individual fish \code{numeric}}
-#'   \item{speciescode}{ \code{numeric}}
+#'   \item{speciescode}{ICCAT 3 letter species codes \code{character}}
 #'   \item{year}{historical assessment year \code{numeric}}
 #'   \item{stock}{biomaass relative to BMSY \code{numeric}}   
 #'   \item{harvest}{exploitation relative to FMSY \code{numeric}}
 #' }
 #' 
 #' @references \url{http://http://iccat.int/Documents/CVSP/CV070_2014/n_4/CV070041365.pdf/}
-#' @name yft
+#' @name releases
 #' @docType data
-#' @author Laurence Kell \email{laurie@@kell.es}
+#' @author Doug Beare \email{doug.beare@@gmail.com}
 #' @keywords data
 NULL
