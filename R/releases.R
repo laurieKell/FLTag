995295b3fab84ff144baa2d85ea535512428d9ec
bearedo <doug.beare@gmail.com>
2016-11-15 14:47:58 +0100

    Pruned datasets, started manual for release file

---
+++ R/releases.R
@@ -0,0 +1,20 @@
+#' releases
+#' 
+#' An example dataset from Atlantic Ocean Tropical Tuna Programmes. Releases of tropical tuna in the Atlantic Ocean. 
+#'
+#' @format A \code{data.frame}
+#' \describe{
+#'   \item{sendingid}{Sending ID to identify person who sent the data \code{numeric}}
+#'   \item{specimenid}{Code for individual fish \code{numeric}}
+#'   \item{speciescode}{ \code{numeric}}
+#'   \item{year}{historical assessment year \code{numeric}}
+#'   \item{stock}{biomaass relative to BMSY \code{numeric}}   
+#'   \item{harvest}{exploitation relative to FMSY \code{numeric}}
+#' }
+#' 
+#' @references \url{http://http://iccat.int/Documents/CVSP/CV070_2014/n_4/CV070041365.pdf/}
+#' @name yft
+#' @docType data
+#' @author Laurence Kell \email{laurie@@kell.es}
+#' @keywords data
+NULL
