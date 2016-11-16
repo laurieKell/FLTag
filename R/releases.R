b7d954c4b9c4b98faadafce911b416020ebeb3ed
bearedo <doug.beare@gmail.com>
2016-11-16 14:19:01 +0100

    releases

--- R/releases.R
+++ R/releases.R
@@ -8,13 +8,13 @@
 #'   \item{rcstagecode}{Stage \code{factor}}
 #'   \item{ctcode1}{ Conventional tag code 1\code{numeric}}
 #'   \item{ctcode2}{ Conventional tag code 2\code{numeric}}
-#'   \item{rel_latitude}{Latitude of release \code{numeric}}
-#'   \item{rel_longitude}{Longitude of release \code{numeric}}
-#'   \item{rel_gearcode}{Gearcode used by release platform \code{factor}}
-#'   \item{rel_depth}{Depth at release \code{numeric}}
-#'   \item{rel_len}{Length of fish recorded at release \code{numeric}}
+#'   \item{latitude}{Latitude of release \code{numeric}}
+#'   \item{longitude}{Longitude of release \code{numeric}}
+#'   \item{gearcode}{Gearcode used by release platform \code{factor}}
+#'   \item{depth}{Depth at release \code{numeric}}
+#'   \item{len}{Length of fish recorded at release \code{numeric}}
 #'   \item{specimenid}{Unique fish ID \code{numeric}}
-#'   \item{rel_date}{Release date \code{date}}
+#'   \item{date}{Release date \code{date}}
 #' }
 #' @references \url{http://http://iccat.int/Documents/CVSP/CV070_2014/n_4/CV070041365.pdf/}
 #' @name releases
