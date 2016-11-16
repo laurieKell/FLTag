f67037d06dc3bb11b4d2e76626cdcbc46fa1111a
bearedo <doug.beare@gmail.com>
2016-11-16 10:43:42 +0100

    metadata for AOTTP releases table

--- R/recoveries.R
+++ R/recoveries.R
@@ -8,15 +8,12 @@
 #'   \item{rcstagecode}{Stage \code{factor}}
 #'   \item{ctcode1}{ Conventional tag code 1\code{numeric}}
 #'   \item{ctcode2}{ Conventional tag code 2\code{numeric}}
-#'   \item{date}{Recovery date of fish\code{factor}}
-#'   \item{time}{Recovery time of fish \code{factor}}
-#'   \item{latitude}{Latitude of recovery \code{numeric}}
-#'   \item{longitude}{Longitude of recovery \code{numeric}}
-#'   \item{gearcode}{Gearcode used by recovery platform \code{factor}}
-#'   \item{depth}{Depth of release \code{numeric}}
-#'   \item{len}{Length of recovered fish \code{numeric}}
+#'   \item{rec_latitude}{Latitude of recovery \code{numeric}}
+#'   \item{rec_longitude}{Longitude of recovery \code{numeric}}
+#'   \item{rec_gearcode}{Gearcode used by recovery platform \code{factor}}
+#'   \item{rec_len}{Length of recovered fish \code{numeric}}
+#'    \item{rec_date}{Date fish was recovered \code{date}}
 #' }
-#' 
 #' @references \url{http://http://iccat.int/Documents/CVSP/CV070_2014/n_4/CV070041365.pdf/}
 #' @name recoveries
 #' @docType data
