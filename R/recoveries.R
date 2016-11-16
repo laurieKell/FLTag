abb3dfdfa4869b8dd33122aebb54ecdf6aa9e12a
bearedo <doug.beare@gmail.com>
2016-11-16 14:18:51 +0100

    recoveries

--- R/recoveries.R
+++ R/recoveries.R
@@ -8,11 +8,11 @@
 #'   \item{rcstagecode}{Stage \code{factor}}
 #'   \item{ctcode1}{ Conventional tag code 1\code{numeric}}
 #'   \item{ctcode2}{ Conventional tag code 2\code{numeric}}
-#'   \item{rec_latitude}{Latitude of recovery \code{numeric}}
-#'   \item{rec_longitude}{Longitude of recovery \code{numeric}}
-#'   \item{rec_gearcode}{Gearcode used by recovery platform \code{factor}}
-#'   \item{rec_len}{Length of recovered fish \code{numeric}}
-#'    \item{rec_date}{Date fish was recovered \code{date}}
+#'   \item{latitude}{Latitude of recovery \code{numeric}}
+#'   \item{longitude}{Longitude of recovery \code{numeric}}
+#'   \item{gearcode}{Gearcode used by recovery platform \code{factor}}
+#'   \item{len}{Length of recovered fish \code{numeric}}
+#'    \item{date}{Date fish was recovered \code{date}}
 #' }
 #' @references \url{http://http://iccat.int/Documents/CVSP/CV070_2014/n_4/CV070041365.pdf/}
 #' @name recoveries
